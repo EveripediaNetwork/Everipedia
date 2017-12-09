077d788c62e103ceb3801b34be7bb717135605c2
Live session user <kedarmail@gmail.com>
2017-12-09 20:14:35 +0000

    hello world

---
+++ hello/hello.cpp
@@ -0,0 +1,25 @@
+/**
+ *  @file
+ *  @copyright defined in eos/LICENSE.txt
+ */
+#include <hello/hello.hpp>
+
+/**
+ *  The init() and apply() methods must have C calling convention so that the blockchain can lookup and
+ *  call these methods.
+ */
+extern "C" {
+
+    /**
+     *  This method is called once when the contract is published or updated.
+     */
+    void init()  {
+       eosio::print( "Init World!\n" );
+    }
+
+    /// The apply method implements the dispatch of events to this contract
+    void apply( uint64_t code, uint64_t action ) {
+       eosio::print( "Hello World: ", eosio::name(code), "->", eosio::name(action), "\n" );
+    }
+
+} 
