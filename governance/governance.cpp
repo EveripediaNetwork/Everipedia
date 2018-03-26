ad99905a8cec09b6199ea09660e58292ce34af2e
Live session user <kedarmail@gmail.com>
2018-03-26 13:15:48 -0700

    governance updates

--- governance/governance.cpp
+++ governance/governance.cpp
@@ -4,21 +4,25 @@ class governance : public contract {
 private:
     using byte = unsigned char;
     enum Module { token, article, _governance };
-    enum Opinion { Yes, No };
 
     // DB Table Schemas
     struct Proposal {
+        Proposal () {}
+
         Module module;
         std::string file;
+
         std::string primary_key()const { return file; }
     };
 
     struct Stake {
+        Stake () {}
+
         uint64_t id;
         uint64_t proposal_id;
         account_name staker;
         uint64_t amount;
-        Opinion opinion;
+        bool approve;
         bool active;
 
         uint64_t primary_key()const { return id; }
@@ -30,11 +34,17 @@ private:
 public:
 
     void propose( Module module, std::string file ) {
-	
+	    Proposal proposal;
+        proposal.file = file;
+        proposal.module = module;
+        //_proposals.insert( proposal );
     }
 
-    void stake( uint64_t proposal_id ) {
-        
+    void stake( uint64_t proposal_id, bool approve ) {
+        Stake stake;
+        stake.proposal_id = proposal_id;
+        stake.approve = approve;
+        //_stakes.insert( stake );
     }
 
 };
