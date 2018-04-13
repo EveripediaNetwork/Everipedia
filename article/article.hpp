7c74d8326aa673d821539eb8f56b3b311f3f440e
Live session user <kedarmail@gmail.com>
2018-04-13 16:42:57 -0700

    article module rewrite

--- article/article.hpp
+++ article/article.hpp
@@ -32,92 +32,93 @@
 
 using namespace eosio;
 
-class article : public contract {
+class article : public eosio::contract {
 
 private:
-      const int DEFAULT_VOTING_TIME = 28800;
-      const int MINIMUM_EDIT_IQ = 1;
-      const account_name ARTICLE_CONTRACT = N("iqarticlecnt");
+    const int DEFAULT_VOTING_TIME = 86400;
+    const int MINIMUM_EDIT_IQ = 1;
+    const account_name ARTICLE_CONTRACT = N("iqarticlecnt");
 
-      // returning array types from a DB type struct throws
-      // using vectors for now, will try to use arrays later 
-      //using ipfshash_t = unsigned char[34];
-      using byte = unsigned char;
-      using ipfshash_t = std::vector<byte>;
-      enum ProposalStatus { pending, accepted, rejected };
+    // returning array types from a DB type struct throws
+    // using vectors for now, will try to use arrays later 
+    //using ipfshash_t = unsigned char[34];
+    using byte = unsigned char;
+    using ipfshash_t = std::vector<byte>;
+    enum ProposalStatus { pending, accepted, rejected };
 
-      // ==================================================
-      // ==================================================
-      // ==================================================
-      // DATABASE SCHEMAS
+    // ==================================================
+    // ==================================================
+    // ==================================================
+    // DATABASE SCHEMAS
 
-      struct wiki {
-            uint64_t id;
-            ipfshash_t hash; // IPFS hash of the current consensus article version
-            ipfshash_t parent_hash; // IPFS hash of the parent article version
+    struct wiki {
+          uint64_t id;
+          ipfshash_t hash; // IPFS hash of the current consensus article version
+          ipfshash_t parent_hash; // IPFS hash of the parent article version
 
-            // only uint64_t primary keys are allowed for some reason
-            // not sure if that's temporary or a design decision
-            // will update to ipfshash_t primary key later if possible
-            //ipfshash_t primary_key()const { return hash; }
-            uint64_t primary_key () const { return id; }
+          // only uint64_t primary keys are allowed for some reason
+          // not sure if that's temporary or a design decision
+          // will update to ipfshash_t primary key later if possible
+          //ipfshash_t primary_key()const { return hash; }
+          uint64_t primary_key () const { return id; }
 
-            wiki() {}
-            ipfshash_t get_hash () const { return hash; }
-            ipfshash_t get_parent_hash () const { return parent_hash; }
-      };
+          wiki() {}
+          ipfshash_t get_hash () const { return hash; }
+          ipfshash_t get_parent_hash () const { return parent_hash; }
+    };
 
-      // Edit Proposals
-      struct editproposal {
-            uint64_t id;
-            ipfshash_t proposed_article_hash; // IPFS hash of the proposed new version
-            ipfshash_t old_article_hash; // IPFS hash of the old version
-            account_name proposer; // account name of the proposer
-            std::time_t timestamp; // epoch time of the proposal
-            ProposalStatus status;
-            uint64_t primary_key () const { return id; }
+    // Edit Proposals
+    struct editproposal {
+          uint64_t id;
+          ipfshash_t proposed_article_hash; // IPFS hash of the proposed new version
+          ipfshash_t old_article_hash; // IPFS hash of the old version
+          account_name proposer; // account name of the proposer
+          std::time_t timestamp; // epoch time of the proposal
+          ProposalStatus status;
+          uint64_t primary_key () const { return id; }
 
-            editproposal() {}
-            ipfshash_t get_hash () const { return proposed_article_hash; }
+          editproposal() {}
+          ipfshash_t get_hash () const { return proposed_article_hash; }
 
-      };
+    };
 
-      // Voting tally
-      struct vote {
-            uint64_t id;
-            ipfshash_t proposed_article_hash; // IPFS hash of the proposed new version
-            uint64_t votevalue; // positive or negative value indicating the vote / staked amount
-            account_name voter; // account name of the voter
-            std::time_t timestamp; // epoch time of the vote
+    // Voting tally
+    struct vote {
+          uint64_t id;
+          uint64_t proposal_id;
+          uint64_t amount; // positive or negative value indicating the vote / staked amount
+          account_name voter; // account name of the voter
+          std::time_t timestamp; // epoch time of the vote
 
-            uint64_t primary_key()const { return id; }
-            ipfshash_t get_proposed () const { return proposed_article_hash; }
-            vote() {}
-      };
+          uint64_t primary_key()const { return id; }
+          uint64_t get_proposal_id()const { return id; }
+          vote() {}
+    };
 
 
-      //  ==================================================
-      //  ==================================================
-      //  ==================================================
-      // DATABASE TABLES
+    //  ==================================================
+    //  ==================================================
+    //  ==================================================
+    // DATABASE TABLES
 
-      // wikis table
-      // indexed by wiki hash
-      // indexed by parent hash
-      eosio::multi_index<N(wikis), wiki,
-        indexed_by< N(byhash), const_mem_fun< wiki, ipfshash_t, &wiki::get_hash >>,
-        indexed_by< N(byparent), const_mem_fun< wiki, ipfshash_t, &wiki::get_parent_hash >>
-      > wikis_table; // EOS table for the articles
+    // wikis table
+    // indexed by wiki hash - currently not supported
+    // indexed by parent hash - currently not supported
+    typedef eosio::multi_index<N(wikis), wiki
+      //,indexed_by< N(byhash), const_mem_fun< wiki, ipfshash_t, &wiki::get_hash >>
+      //,indexed_by< N(byparent), const_mem_fun< wiki, ipfshash_t, &wiki::get_parent_hash >>
+    > wikis_table; // EOS table for the articles
 
-      // edit proposals table
-      // 12-char limit on table names, so proposals used instead of editproposals
-      // indexed by hash
-      eosio::multi_index<N(proposals), editproposal> edit_proposals_table; // EOS table for the edit proposals
+    // edit proposals table
+    // 12-char limit on table names, so proposals used instead of editproposals
+    // indexed by hash
+    typedef eosio::multi_index<N(proposals), editproposal> edit_proposals_table; // EOS table for the edit proposals
 
-      // votes table
-      eosio::multi_index<N(votes), vote,
-          indexed_by< N(byproposal), const_mem_fun< vote, ipfshash_t, &vote::get_proposed >>
-      > votes_table; // EOS table for the votes
+    // votes table
+    // indexed by proposal
+    typedef eosio::multi_index<N(votes), vote,
+        indexed_by< N(byproposal), const_mem_fun< vote, uint64_t, &vote::get_proposal_id >>
+    > votes_table; // EOS table for the votes
 
 
     // ==================================================
@@ -131,21 +132,21 @@ private:
 
 
 public:
-    article( account_name self ) :contract(self), 
-    wikis_table( _self, _self), edit_proposals_table(_self, _self), votes_table(_self, _self) {}
+    article(account_name self) : contract(self) {}
     //  ==================================================
     //  ==================================================
     //  ==================================================
     // ABI Functions
 
-    void submit_proposal( ipfshash_t& proposed_article_hash, ipfshash_t& old_article_hash ) { 
+    void submit_proposal( account_name proposer, ipfshash_t& proposed_article_hash, ipfshash_t& old_article_hash ) { 
         
         // TODO:if there is a brand new user, give them free IQ to make their first edit
         
-        // TODO: make sure there is enough IQ to make an edit
+        // TODO: Stake IQ
 
         // store the proposal
-        edit_proposals_table.emplace( _self, [&]( auto& a ) {
+        edit_proposals_table proptable( _self, proposer );
+        proptable.emplace( _self, [&]( auto& a ) {
             a.id = 1;
             a.proposed_article_hash = proposed_article_hash;
             a.old_article_hash = old_article_hash;
@@ -154,17 +155,68 @@ public:
             a.status = ProposalStatus::pending;
         });
 
-        // TODO: Stake IQ
     }
 
     // vote on a proposal
-    void place_vote ( const vote& _vote ) {
+    void place_vote ( account_name voter, uint64_t proposal_id, uint64_t amount ) {
+        
+        // Check if article exists
+        edit_proposals_table proptable( _self, voter );
+        auto prop_it = proptable.find( proposal_id );
+        eosio_assert( prop_it != proptable.end(), "proposal not found" );
+
+        // Verify voting is still in progress
+        using chrono = std::chrono::system_clock;
+        std::time_t now = chrono::to_time_t(chrono::now());
+        eosio_assert( now < prop_it->timestamp + DEFAULT_VOTING_TIME, "voting period is over");
+
+        // TODO: Send IQ to contract
+
+        // Store vote in DB
+        votes_table votetbl( _self, voter );
+        votetbl.emplace( voter, [&]( auto& a ) {
+             // TODO: incrementing IDs
+             a.proposal_id = proposal_id;
+             a.amount = amount;
+             a.voter = voter;
+             a.timestamp = now;
+        });
 
     }
 
-    // calculate proposal result after votes are complete
-    void finalize_proposal( const editproposal& _editprop ) {
+    void finalize_proposal( account_name from, uint64_t proposal_id ) {
 
+         // Verify proposal exists
+         edit_proposals_table proptable( _self, _self );
+         auto prop_it = proptable.find( proposal_id );
+         eosio_assert( prop_it != proptable.end(), "proposal not found" );
+
+         // Verify voting period is complete
+          using chrono = std::chrono::system_clock;
+          std::time_t now = chrono::to_time_t(chrono::now());
+          eosio_assert( now > prop_it->timestamp + DEFAULT_VOTING_TIME, "voting period is over");
+
+         // TODO: Retrieve votes from DB
+
+         // TODO: Tally votes
+
+
+         // TODO: Mark proposal as accepted or rejected. Ties are rejected
+         proptable.modify( prop_it, 0, [&]( auto& a ) {
+             a.status =  ProposalStatus::accepted;
+         });
+
+         // TODO: Reward the pro votes
+
+         // TODO: Punish the anti votes
+
+         // TODO: Add article to database
+         wikis_table wikitbl( _self, _self );
+         wikitbl.emplace( _self,  [&]( auto& a ) {
+             // TODO: incrementing ID
+             a.hash = prop_it->proposed_article_hash;
+             a.parent_hash = prop_it->old_article_hash;
+         });
     }
 
 
