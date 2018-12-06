7355a3bb450a73b7bd4f6d38cf49895194b782a6
kedar <kedarmail@gmail.com>
2018-12-05 17:54:51 -0800

    iqsafesendiq new CDT version compiling

--- iqsafesendiq/iqsafesendiq.cpp
+++ iqsafesendiq/iqsafesendiq.cpp
@@ -1,24 +1,33 @@
-#include <everipediaiq/everipediaiq.hpp>
 #include <eosiolib/eosio.hpp>
+#include <eosiolib/asset.hpp>
 
-void safetransfer (const everipediaiq::transfer_args& transfer) {
-    if (transfer.to != N(iqsafesendiq))
+using namespace eosio;
+
+struct transfer_args {
+    name   from;
+    name   to;
+    asset  quantity;
+    std::string memo;
+};
+
+void safetransfer (const transfer_args& transfer) {
+    if (transfer.to != name("iqsafesendiq"))
         eosio_exit(0);
 
     print("forward to:", transfer.memo, ":");
-    account_name to = eosio::string_to_name(transfer.memo.c_str());
+    name to = name(transfer.memo);
     print("forward to:", to);
     action(
-        permission_level{ N(iqsafesendiq), N(active) }, 
-        N(everipediaiq), N(transfer),
-        std::make_tuple( N(iqsafesendiq), to, transfer.quantity, std::string("safetransfer") )
+        permission_level{ name("iqsafesendiq"), name("active") }, 
+        name("everipediaiq"), name("transfer"),
+        std::make_tuple( name("iqsafesendiq"), to, transfer.quantity, std::string("safetransfer") )
     ).send();
 }
 
 extern "C" {
     [[noreturn]] void apply( uint64_t receiver, uint64_t code, uint64_t action ) {
-        eosio_assert( code == N(everipediaiq) && action == N(transfer), "only IQ transfers permitted");   
-        safetransfer( unpack_action_data<everipediaiq::transfer_args>() );
+        eosio_assert( code == name("everipediaiq").value && action == name("transfer").value, "only IQ transfers permitted");   
+        safetransfer( unpack_action_data<transfer_args>() );
         eosio_exit(0);
     }
 }
