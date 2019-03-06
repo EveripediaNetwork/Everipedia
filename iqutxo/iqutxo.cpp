8d2a77a15b6da8f887720ac10ee6649686901a5e
kedar <kedarmail@gmail.com>
2019-03-05 22:37:48 -0800

    remove debug function

--- iqutxo/iqutxo.cpp
+++ iqutxo/iqutxo.cpp
@@ -1,18 +1,6 @@
 #include "iqutxo.hpp"
 
 
-std::string bytetohex(unsigned char *data, int len) {
-    constexpr char hexmap[] = {'0', '1', '2', '3', '4', '5', '6', '7',
-                               '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
-
-    std::string s(len * 2, ' ');
-    for (int i = 0; i < len; ++i) {
-        s[2 * i]     = hexmap[(data[i] & 0xF0) >> 4];
-        s[2 * i + 1] = hexmap[data[i] & 0x0F];
-    }
-    return s;
-}
-
 [[eosio::action]]
 void iqutxo::create(name issuer, asset maximum_supply) {
     require_auth( _self );
@@ -108,7 +96,6 @@ void iqutxo::transfer(
     memcpy(rawtx + 76, (uint8_t *)&fee.amount, 8);
     memcpy(rawtx + 84, (uint8_t *)&nonce, 8);
     memcpy(rawtx + 92, memo.c_str(), memo.size());
-    print(bytetohex(rawtx, length));
     
     // hash transaction
     checksum256 digest = sha256((const char *)rawtx, length);
