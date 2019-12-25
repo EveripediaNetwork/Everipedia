836a02870b4692a78e31f5fa432db7784c301ad7
travis <travis@everipedia.com>
2019-12-24 18:13:29 -0800

    Guild stuff: Part 1

--- build.sh
+++ build.sh
@@ -18,6 +18,11 @@ cd eparticlectr
 rm -f eparticlectr.abi eparticlectr.wasm eparticlectr.wast
 cd ..
 
+echo "Deleting old iqdelegation files..."
+cd iqdelegation
+rm -f iqdelegation.abi iqdelegation.wasm iqdelegation.wast
+cd ..
+
 cd everipediaiq
 echo "Building everipediaiq..."
 /usr/bin/eosio-cpp everipediaiq.cpp -O=3 -lto-opt=O3 -stack-size=16384 -abigen -o everipediaiq.wasm -I everipediaiq.clauses.md -I everipediaiq.contracts.md -I ../eparticlectr
@@ -28,5 +33,11 @@ echo "Building eparticlectr..."
 /usr/bin/eosio-cpp eparticlectr.cpp -O=3 -lto-opt=O3 -stack-size=16384 -abigen -o eparticlectr.wasm -I eparticlectr.clauses.md -I eparticlectr.contracts.md
 cd ..
 
+cd iqdelegation
+echo "Building iqdelegation..."
+/usr/bin/eosio-cpp iqdelegation.cpp -O=3 -lto-opt=O3 -stack-size=16384 -abigen -o iqdelegation.wasm -I iqdelegation.clauses.md -I iqdelegation.contracts.md
+cd ..
+
 cleos set contract everipediaiq ./everipediaiq/
 cleos set contract eparticlectr ./eparticlectr/
+cleos set contract iqdelegation ./iqdelegation/
